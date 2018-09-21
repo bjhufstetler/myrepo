83b64e73276042b76e98531145548732bd8850a5
bjhufstetler <bjhufstetler@gmail.com>
2018-09-20 21:07:49 -0400

    oops, this is the real code

---
+++ RPractice.R
@@ -0,0 +1,32 @@
+1 + 2
+height <- 3
+height
+width <-6
+width
+area <- height * width
+area
+ls()
+TRUE
+class(TRUE)
+2
+2.5
+2L
+class(2L)
+class(2)
+is.numeric(2)
+a <- "I love data science"
+a
+class(a)
+as.numeric(TRUE)
+as.numeric(FALSE)
+as.character(4)
+
+
+suits <- c("Spades", "Hearts", "Diamonds", "Clubs")
+remain <- c(11, 12, 11, 13)
+
+names(remain) <- suits
+remain
+remain[1]
+remain[c(1, 4)]
+remain[c(TRUE, FALSE)]
