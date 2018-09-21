9c3c886af49f4d15be736c7c90cbb5306ec1c2bb
bjhufstetler <bjhufstetler@gmail.com>
2018-09-20 21:22:16 -0400

    1

--- RPractice.R
+++ RPractice.R
@@ -30,3 +30,27 @@ remain
 remain[1]
 remain[c(1, 4)]
 remain[c(TRUE, FALSE)]
+
+#Matrices
+
+matrix(1:6, nrow = 2)
+matrix(1:6, ncol = 3)
+
+matrix(1:3, ncol = 2, nrow = 3, byrow = TRUE)
+
+cbind(1:3, 1:3)
+rbind(1:3, 1:3)
+
+m <- matrix(1:6, byrow = TRUE, nrow = 2)
+m
+rbind(m, 7:9)
+cbind(m, c(10, 11))
+rownames(m) <- c("row1", "row2")
+m
+colnames(m) <- c("col1", "col2", "col3")
+m
+
+n <- matrix(1:6, byrow = TRUE, nrow = 2,
+            dimnames = list(c("row1", "row2"),
+                            c("col1", "col2", "col3")))
+n
