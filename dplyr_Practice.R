3a76268a0bbe969647543f1f6ed9bcbaff958af1
bjhufstetler <bjhufstetler@gmail.com>
2018-09-21 23:30:48 -0400

    lots more added

--- dplyr_Practice.R
+++ dplyr_Practice.R
@@ -97,3 +97,157 @@ ggplotly()
  #    title = 'Commercial Airports Worldwide', geo = g
  #  )
 
+
+# Monthly totals of accidental deaths in the USA
+plot_ly(x = time(USAccDeaths), y = USAccDeaths) %>% 
+  add_lines() %>%
+  rangeslider()
+
+# Apple Stock Price With Rangeslider
+  #plot_ly(apple_stock_price, x = ~Date) %>%
+  #  add_lines(y = ~AAPL.Adjusted, name = "Apple") %>% 
+  #  rangeslider() %>% 
+  #  layout(
+  #    title = "Stock Price Apple",
+  #    xaxis = list(title = "Date"),
+  #    yaxis = list(title = "Price"))
+
+# Explore the mtcars data frame with str()
+str(mtcars)
+# Execute the following command
+ggplot(mtcars, aes(x = cyl, y = mpg)) +
+  geom_point()
+
+# Change the command below so that cyl is treated as factor
+ggplot(mtcars, aes(x = factor(cyl), y = mpg)) +
+  geom_point()
+
+# A scatter plot has been made for you
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point()
+
+# Color Dependency
+ggplot(mtcars, aes(x = wt, y = mpg, color = disp)) +
+  geom_point()
+
+# Size Dependency
+ggplot(mtcars, aes(x = wt, y = mpg, size = disp)) +
+  geom_point()
+
+# Add geom_point() with +
+ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_point()
+
+# Add geom_point() and geom_smooth() with +
+ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_smooth() +
+  geom_point()
+
+# 1 - The plot you created in the previous exercise
+ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_point() +
+  geom_smooth()
+
+# 2 - Copy the above command but show only the smooth line
+ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_smooth()
+
+
+# 3 - Copy the above command and assign the correct value to col in aes()
+ggplot(diamonds, aes(x = carat, y = price, col = clarity)) +
+  geom_smooth()
+
+
+# 4 - Keep the color settings from previous command. Plot only the points with argument alpha.
+ggplot(diamonds, aes(x = carat, y = price, col = clarity)) +
+  geom_point(alpha = 0.4)
+
+# Create the object containing the data and aes layers: dia_plot
+dia_plot <- ggplot(diamonds, aes(x = carat, y = price))
+
+# Add a geom layer with + and geom_point()
+dia_plot + geom_point()
+
+# Add the same geom layer, but with aes() inside
+dia_plot + geom_point(aes(color = clarity))
+
+# 1 - The dia_plot object has been created for you
+dia_plot <- ggplot(diamonds, aes(x = carat, y = price))
+
+# 2 - Expand dia_plot by adding geom_point() with alpha set to 0.2
+dia_plot <- dia_plot + geom_point(alpha = 0.2)
+
+# 3 - Plot dia_plot with additional geom_smooth() with se set to FALSE
+dia_plot + geom_smooth(se = FALSE)
+
+# 4 - Copy the command from above and add aes() with the correct mapping to geom_smooth()
+dia_plot + geom_smooth(aes(col = clarity), se = FALSE)
+
+
+
+
+
+
+
+#If/else statements
+# Variables related to your last day of recordings
+medium <- "LinkedIn"
+num_views <- 14
+
+# Control structure for medium
+if (medium == "LinkedIn") {
+  print("Showing LinkedIn information")
+} else if (medium == "Facebook") {
+  # Add code to print correct string when condition is TRUE
+  print("Showing Facebook information")
+} else {
+  print("Unknown medium")
+}
+
+# Control structure for num_views
+if (num_views > 15) {
+  print("You're popular!")
+} else if (num_views <= 15 & num_views > 10) {
+  # Add code to print correct string when condition is TRUE
+  print("Your number of views is average")
+} else {
+  print("Try to be more visible!")
+}
+
+
+
+
+# ggplot2 round 2
+
+# Explore the mtcars data frame with str()
+str(mtcars)
+
+# A scatter plot with LOESS smooth
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() + geom_smooth()
+
+# A scatter plot with an ordinary Least Squares linear model
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() + geom_smooth(method = "lm")
+
+# The previous plot, without CI ribbon
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point() + geom_smooth(method = "lm", se = FALSE)
+
+# The previous plot, without points
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_smooth(method = "lm", se = FALSE)
+
+
+
+#Multiple lines
+# 1 - Define cyl as a factor variable
+ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE)
+
+# 2 - Plot 1, plus another stat_smooth() containing a nested aes()
+ggplot(mtcars, aes(x = wt, y = mpg, col = factor(cyl))) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE) +
+  stat_smooth(method = "lm", se = FALSE, aes(group = 1))
